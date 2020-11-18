b674b4e916eef2f6d3cd213611d66cd2a037d796
abdoul <abdoulazizzorom@gmail.com>
2020-11-18 10:57:54 +0000

    config file update

--- config/govpayclient.php
+++ config/govpayclient.php
@@ -7,20 +7,7 @@ return [
 
     "payementMethods" => [
         "om" => "76752085",
-
         "mc" => "70000000",
-
-        "pp" => [
-            'client_id' => "AT_i6U7IuzTABpvn-2lMuJ1svbTtqqe2La4jeynXLm8I2O3lHj4nJZIcQu8JoUYkQzHtoFuKI-8ZkCKX",
-            'secret' => "EEguq1my8A1HVK7UlAb5IeD5jZFbH7nmqpdMU5_aBjwhV-mhBSF9Lm3bz1kS5n4SWcIefyL9BNuv1sY4",
-            'settings' => array(
-                'mode' => "sandbox",
-                'http.ConnectionTimeOut' => 30,
-                'log.LogEnabled' => true,
-                'log.FileName' => storage_path() . '/logs/paypal.log',
-                'log.LogLevel' => 'ERROR'
-            ),
-        ]
     ],
 
 ];
